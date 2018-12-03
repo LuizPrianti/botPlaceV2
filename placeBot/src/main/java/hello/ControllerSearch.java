800aa9efa6ebadb6551a9dff8802e3687bcb738a
Luiz Fernando Prianti <37250746+LuizPrianti@users.noreply.github.com>
2018-12-03 10:31:19 -0200

    Add files via upload

--- placeBot/src/main/java/hello/ControllerSearch.java
+++ placeBot/src/main/java/hello/ControllerSearch.java
@@ -1,8 +1,10 @@
-package hello;
+package app;
 import com.pengrad.telegrambot.model.Update;
 
 public interface ControllerSearch {
 	
 	public void search(Update update);
+	
+	public void searchAPI(String latlon, String categoria) throws IOException;
 
 }
